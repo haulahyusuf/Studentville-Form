764f6299af467958683bb0e49b27a128f79b159f
Haulah Yusuf <mukamihaulah@gmail.com>
2024-03-19 13:09:19 +0300

    alertprompt

--- alert.js
+++ alert.js
@@ -1,5 +1,4 @@
 document.getElementById("registerBtn").addEventListener("click", function() {
-    // Get the values from the form fields
     var name = document.getElementById("name").value;
     var dob = document.getElementById("dob").value;
     var address = document.getElementById("address").value;
@@ -8,12 +7,8 @@ document.getElementById("registerBtn").addEventListener("click", function() {
     var gender = document.querySelector("input[name='gender']:checked").value;
     var course = document.getElementById("course").value;
 
-    // Perform validation (you can add your own validation logic here)
-
-    // Display a confirmation message
     alert("Thank you, " + name + ", for registering! We'll keep in touch.");
 
-    // Perform additional action based on the selected course
     var selectedCourse = document.getElementById("course").value;
-    alert("You selected: " + selectedCourse); // For demonstration, you can replace this with any desired functionality
+    alert("You selected: " + selectedCourse);
 });
